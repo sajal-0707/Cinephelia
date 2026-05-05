5c44d3eb2631c86c3fd5e0aec93e4d129037fab4
Sajal Chourasia <sajal007@Sajals-MacBook-Air.local>
2026-05-05 23:21:28 +0530

    fix localhost in Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
 
   const handleGoogleLogin = () => {
     setIsLoggingIn(true);
-    window.location.href = 'http://localhost:4000/api/auth/google';
+    window.location.href = 'https://cinephelia-backend.onrender.com/api/auth/google';
   };
 
   return (
